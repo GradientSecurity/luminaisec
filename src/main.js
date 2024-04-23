1db513cde094e06fbfa6d8ffd5c2c4325fcf1fdf
Guy Nachshon <58976716+GuyNachshon@users.noreply.github.com>
2024-04-23 23:59:20 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import VueKinesis from "vue-kinesis";
 
-createApp(App).use(VueKinesis).mount('#app')
+createApp(App).mount('#app')
